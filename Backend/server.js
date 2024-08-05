a270197317ff7591c416302186aae8c0cd1a5ad7
sravantorra1100 <sravantorra1100@gmail.com>
2024-08-05 14:16:32 +0530

    commit 1: server created

---
+++ Backend/server.js
@@ -0,0 +1,25 @@
+const express=require('express');
+const morgan=require('morgan');
+const cors=require('cors');
+const colors=require('colors');
+const bodyParser=require('body-parser');
+const dotenv=require('dotenv')
+
+//dotenv 
+dotenv.config()
+
+//rest object
+const app=express()
+const port=process.env.PORT || 3000
+
+
+//middlewares
+app.use(cors())
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(morgan('dev'))
+
+//listen server
+app.listen(port,()=>{
+    console.log(`server running on port ${port}`.bgYellow)
+});
